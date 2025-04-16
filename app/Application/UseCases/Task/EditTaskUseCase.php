5df5c45d2cd9648d432c3c8b5ac58792856de565
o_khirano <hirano@kairosmarketing.net>
2025-04-16 11:35:52 -0700

    update comment

--- app/Application/UseCases/Task/EditTaskUseCase.php
+++ app/Application/UseCases/Task/EditTaskUseCase.php
@@ -6,7 +6,6 @@ class EditTaskUseCase
 {
     public function execute(int $taskId, array $data)
     {
-        // タスク編集のビジネスロジックを実装
         // 例：タスクの検索、バリデーション、更新処理など
         
         return true;

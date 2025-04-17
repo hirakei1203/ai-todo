4e173675dd9fdfead20c067826e1d53bf901abec
o_khirano <hirano@kairosmarketing.net>
2025-04-17 10:42:45 -0700

    update comment

--- app/Application/UseCases/Task/CreateTaskUseCase.php
+++ app/Application/UseCases/Task/CreateTaskUseCase.php
@@ -6,7 +6,6 @@ class CreateTaskUseCase
 {
     public function execute(array $data)
     {
-        // タスク作成のビジネスロジックを実装
         // 例：バリデーション、タスクエンティティの作成、リポジトリでの保存など
         
         return true;
--- app/Application/UseCases/Task/DeleteTaskUseCase.php
+++ app/Application/UseCases/Task/DeleteTaskUseCase.php
@@ -6,9 +6,6 @@ class DeleteTaskUseCase
 {
     public function execute(int $taskId)
     {
-        // タスク削除のビジネスロジックを実装
-        // 例：タスクの検索、削除権限の確認、削除処理など
-        
         return true;
     }
 } 
